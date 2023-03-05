17a4e79ad2656323ba0053e24f2dfa53ed2ae580
tronghaiit2 <tronghaitoank25cbg@gmail.com>
2023-03-05 13:59:11 +0700

    project_release

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo apt-get install build-essential cmake git pkg-config libboost-dev \
+   libboost-date-time-dev libboost-system-dev libboost-filesystem-dev \
+   libboost-thread-dev libboost-chrono-dev libboost-serialization-dev \
+   libboost-program-options-dev libboost-test-dev liblog4cpp5-dev \
+   libuhd-dev gnuradio-dev gr-osmosdr libblas-dev liblapack-dev \
+   libarmadillo-dev libgflags-dev libgoogle-glog-dev libhdf5-dev \
+   libgnutls-openssl-dev libmatio-dev libpugixml-dev libpcap-dev \
+   libprotobuf-dev protobuf-compiler libgtest-dev googletest \
+   python3-mako python3-six
